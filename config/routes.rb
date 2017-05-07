7cee9babcdd98d6229d212ab1bc5b3bc805e902f
Gabe Payne <gabrielmpayne@gmail.com>
2017-05-07 00:19:26 -0500

    Integrated skills from model generator

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   get 'pages/about'
 
-  get 'pages/ontact'
-
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
