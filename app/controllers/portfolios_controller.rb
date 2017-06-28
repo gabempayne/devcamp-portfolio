6ce9df39bcf21207eb4c8f75005f7c78e9b2194e
Gabe Payne <gabrielmpayne@gmail.com>
2017-06-28 18:54:12 -0500

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,16 +1,17 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show ,:destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
-		@portfolio_items = Portfolio.by_position
+    @portfolio_items = Portfolio.by_position
   end
 
   def sort
     params[:order].each do |key, value|
       Portfolio.find(value[:id]).update(position: value[:position])
     end
+
     render nothing: true
   end
 
@@ -18,12 +19,14 @@ class PortfoliosController < ApplicationController
     @angular_portfolio_items = Portfolio.angular
   end
 
-	def new
-		@portfolio_item = Portfolio.new
+  def new
+    @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-	end
+  end
 
-	def create
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
@@ -39,7 +42,7 @@ class PortfoliosController < ApplicationController
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -50,12 +53,10 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # will perform the lookup
-
-    #destroy/ delete record
+    # Destroy/delete the record
     @portfolio_item.destroy
 
-    # will redirect
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
@@ -67,12 +68,13 @@ class PortfoliosController < ApplicationController
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
-                                      )
+    )
   end
 
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-  	
 end
